c0b226a14745adb56a6abd583fe4f0beabc17cba
bscaetano <139719932+bscaetano@users.noreply.github.com>
2023-10-22 18:08:51 -0300

    Create Diagrama.java

---
+++ Diagrama.java
@@ -0,0 +1,26 @@
+
+class iPhone {
+}
+
+class ReprodutorMusical {
+  + tocar()
+  + pausar()
+  + selecionarMusica()
+}
+
+class AparelhoTelefônico {
+  + ligar()
+  + atender()
+  + iniciarCorreiroVoz()
+}
+
+class NavegadorNaInternet {
+  + exibirPagina()
+  + adicionarNovaAba()
+  + atualizarPagina()
+}
+
+//Representação das imteracoes do modelo
+//iPhone "1" *-- "1" ReprodutorMusical
+//iPhone "1" *-- "1" AparelhoTelefônico
+//iPhone "1" *-- "1" NavegadorNaInternet
